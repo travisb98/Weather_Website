57efbffbacae6f9b3d92716639c92a675ddd86c3
unknown <travisbostick@gmail.com>
2020-12-06 14:06:40 -0600

    created python program that converts csv to html

---
+++ WebVisualizations/csv_to_html.py
@@ -0,0 +1,13 @@
+import pandas as pd 
+
+
+#CSV path
+cities_path="WebVisualizations/Resources/cities.csv"
+# reading the csv path to a df
+cities_df = pd.read_csv(cities_path, encoding="UTF-8")
+
+  
+# to save as html file 
+# named as "Table" 
+cities_df.to_html("WebVisualizations/Resources/city.html") 
+  

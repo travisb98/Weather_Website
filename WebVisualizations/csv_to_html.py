9131c3c5f86f5c66b30541e3d17c9932bb35110b
unknown <travisbostick@gmail.com>
2020-12-06 15:37:28 -0600

    completed basic version of data page

--- WebVisualizations/csv_to_html.py
+++ WebVisualizations/csv_to_html.py
@@ -6,8 +6,9 @@ cities_path="WebVisualizations/Resources/cities.csv"
 # reading the csv path to a df
 cities_df = pd.read_csv(cities_path, encoding="UTF-8")
 
+
   
 # to save as html file 
 # named as "Table" 
-cities_df.to_html("WebVisualizations/Resources/city.html") 
+cities_df.to_html("WebVisualizations/Resources/city.html",index=False) 
   
